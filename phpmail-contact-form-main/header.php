8a22422478c6b6d69593a5229b779a2031a80dc7
sompong59 <102735484+sompong59@users.noreply.github.com>
2025-03-06 13:18:32 +0700

    add header

---
+++ phpmail-contact-form-main/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<head>
+    <?php
+    $pageTitle = "ໜ້າເວັບຂອງທ່ານ - " . $currentPage;
+    echo "<title>" . $pageTitle . "</title>";
+    ?>
+</head>
+<body>
+    <div class="wrapper">
+       
+        <a href="#" class="button">
+           <div class="icon">
+               <i class="fab fa-whatsapp"></i>
+           </div>
+           <span>Whatsapp</span>
+        </a>
+   
+     </div>
+     <header class="header">
+        <div class="containerheader">
+            <div class="logo">
+                <img src="th/images/" alt="TechArt Logo">
+            </div>
+            <div class="right-section">
+                
+                <div class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                </div>
+                <nav class="nav">
+                    <ul>
+                        <li><a href="#">ໜ້າຫຼັກ</a></li>
+                        <li><a href="#">ກ່ຽວກັບພວກເຮົາ</a></li>
+                        <li><a href="#">ບໍລິການ</a></li>
+                        <li><a href="#">ລາຄາ</a></li>
+                        <li><a href="#">ຕິດຕໍ່ພວກເຮົາ</a></li>
+                        <button class="language-button" data-lang="la">
+                            <img src="th/images/lao.png" alt="ພາສາລາວ" class="flag-icon">
+                            <span class="language-text">ລາວ</span>
+                        </button>
+                        <div class="button1">
+                            <a href="#" class="request-quote">ຂໍລາຄາ</a>
+                            
+                        </div>
+                    </ul>
+                </nav>
+                
+            </div>
+        </div>
+    </header>
+</body>
+</html>
